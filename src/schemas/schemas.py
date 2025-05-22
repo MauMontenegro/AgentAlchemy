2bd4490391ef7f8c2ee0c7809e61479f2f562219
mauro <m.montenegro.meza@gmail.com>
2025-05-22 15:31:07 -0700

    Update: NesAPI parameters

--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -20,7 +20,7 @@ class ScraperAgentState(TypedDict):
     summary: Annotated[str,"Summary of Article"]   
 
 class NewsApiParams(BaseModel):
-    q: str = Field(description="1-3 concise keyword search terms that are not too specific")
+    q: str = Field(description="User Query")
     #sources: str =Field(description="comma-separated list of sources from: 'abc-news,abc-news-au,associated-press,australian-financial-review,axios,bbc-news,bbc-sport,bloomberg,business-insider,cbc-news,cbs-news,cnn,financial-post,fortune'")
     from_param: str = Field(description="date in format 'YYYY-MM-DD' Two days ago minimum. Extend up to 30 days on second and subsequent requests.")
     to: str = Field(description="date in format 'YYYY-MM-DD' today's date unless specified")
